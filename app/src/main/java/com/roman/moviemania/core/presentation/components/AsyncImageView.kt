10e543e5863e2acd67b00c98c8da303134b69e39
Roman <roma321m@gmail.com>
2024-12-04 10:44:57 +0200

    Style AsyncImageView with placeholder
    - Added a background color to the placeholder image.
    - Tinted the placeholder image with `onSecondaryContainer` color.
    - Used `surfaceContainer` color as the background color.

--- app/src/main/java/com/roman/moviemania/core/presentation/components/AsyncImageView.kt
+++ app/src/main/java/com/roman/moviemania/core/presentation/components/AsyncImageView.kt
@@ -1,10 +1,12 @@
 package com.roman.moviemania.core.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
@@ -73,7 +76,10 @@ fun AsyncImageView(
                     )
                 } else {
                     Image(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.surfaceContainer),
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondaryContainer),
                         painter = painterResource(R.drawable.ic_image),
                         contentDescription = description,
                         contentScale = ContentScale.Fit
